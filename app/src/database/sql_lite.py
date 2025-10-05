24a2e70c28d6ebd577a2a642a782b8ecae96eea6
dreamermx123 <dzargo1@yandex.ru>
2025-10-05 23:58:47 +0300

    create migrations

---
+++ app/src/database/sql_lite.py
@@ -0,0 +1,32 @@
+from sqlalchemy import func
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    AsyncAttrs,
+    async_sessionmaker,
+    create_async_engine,
+    AsyncSession,
+)
+from typing import AsyncGenerator
+
+database_url = "sqlite+aiosqlite:///db.sqlite3"
+engine = create_async_engine(url=database_url)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
+
+
+async def get_session_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
